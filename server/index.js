2d393a7cd7d4ceb838e0a90ee93e72e3061b9ecf
chuagnes <agnes.p.chu@gmail.com>
2017-05-02 23:07:39 -0700

    fixed client rendering

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ var app = express();
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/../client/dist'))
+app.use(express.static(__dirname + '/../public'))
+// app.use('/bundles', express.static(__dirname + '/../bundles'))
+
 
 app.get('/', function(req, res) {
       res.send('hello world');

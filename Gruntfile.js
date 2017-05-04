eb4f385716d820b8941465eed247141b8cd06862
Airyque Ervin <airyque@gmail.com>
2017-05-04 15:35:30 -0700

    attempiting deployment once more

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,19 +10,31 @@ module.exports = function(grunt) {
     },
 
     shell: {
-        multiple: {
-          command: [
-            'npm run react-dev'
-          ].join('&')
-        }
+      multiple: {
+        command: [
+          'npm run build',
+          'grunt nodemon'
+        ].join('&')
       }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'public/*.js',
+        dest: 'public/built.js'
+      }
+    }
 
 
   });
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('build', ['shell']);
+  grunt.registerTask('build', ['shell', 'uglify']);
 
   grunt.registerTask('start', ['shell']);
 };

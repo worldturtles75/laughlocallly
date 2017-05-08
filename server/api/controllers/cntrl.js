b5fd0e6d5171f8d136e46c07e0b9fa964badec84
ebeedle <ebeedle@ucla.edu>
2017-05-07 22:27:47 -0700

    built some routes for comedians and fixed webpack

--- server/api/controllers/cntrl.js
+++ server/api/controllers/cntrl.js
@@ -16,5 +16,13 @@ module.exports.getVenues = function(req, res) {
   var queryString = 'SELECT * FROM venues';
   db.query(queryString, function (err, result){
     res.json(result);
-  })
+  })	
+};
+
+module.exports.getComedians = function(req, res) {
+	var queryString = `SELECT username FROM comedians where 
+	(username = ${req.body.username});`
+	db.query(queryString, function(err, result) {
+		res.json(result);
+	})
 };
--- server/api/routes/rts.js
+++ server/api/routes/rts.js
@@ -1,12 +1,13 @@
-var express = require('express')
-var router = express.Router()
-var cntrl = require('../controllers/cntrl.js')
+var express = require('express');
+var router = express.Router();
+var cntrl = require('../controllers/cntrl.js');
 
 router.get('/', function (req, res) {
-  res.send('Hello World from root')
-})
+  res.send('Hello World from root');
+});
 
 router.get('/getEvents', cntrl.getEvents);
 router.get('/getVenues', cntrl.getVenues);
+router.get('/getComedians', cntrl.getComedians);
 
-module.exports = router
+module.exports = router;

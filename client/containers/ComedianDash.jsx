1846a5b4bc7b4b6f5d9527074ce43df687d8da6e
ebeedle <ebeedle@ucla.edu>
2017-05-13 18:39:30 -0700

    did conditional rendering on ComedianDash which fixed state issue

--- client/containers/ComedianDash.jsx
+++ client/containers/ComedianDash.jsx
@@ -8,43 +8,45 @@ class ComedianDash extends React.Component{
     super(props);  
   
     this.state = {
-      comedianInfo: this.props.location.state.comedianInfo
     }
   }
 
   render () {
-    return (
-      <BrowserRouter>
-       <div className="container"> 
-          <nav className="navbar navbar-lower comedianNav">
-            <div className="navbar-header">
-              <a className="navbar-brand navbar-left" href="/"> Welcome, {this.state.comedianInfo.name}! </a>
-            </div>
-            <div className="container-fluid navbar-right">
-              <ul className="nav navbar-nav">
-                <li> <Link to="/editcomedianprofile"> Edit Profile </Link> </li>   
-                <li> <Link to="/bookvenue"> Open Gigs </Link> </li>      
-                <li> <Link 
-                        to={{
-                          pathname: "/manageevents",
-                          state: {comedianInfo: this.state.comedianInfo}
-                        }}
-                      > Manage Events </Link> </li>
-              </ul>
-            </div>
-          </nav>
-        
-        
-        <Route path="/bookvenue" component={BookVenuePage} />
-        <Route path="/manageevents" component={ManageEventsPage} />
+    const loggedIn = !!this.props.location.state;
+    if (loggedIn) {
+      return (
+        <BrowserRouter>
+         <div className="container"> 
+            <nav className="navbar navbar-lower comedianNav">
+              <div className="navbar-header">
+                <a className="navbar-brand navbar-left" href="/"> Welcome, {this.props.location.state.comedianInfo.name}! </a>
+              </div>
+              <div className="container-fluid navbar-right">
+                <ul className="nav navbar-nav">
+                  <li> <Link to="/editcomedianprofile"> Edit Profile </Link> </li>   
+                  <li> <Link to="/bookvenue"> Open Gigs </Link> </li>      
+                  <li> <Link 
+                          to={{
+                            pathname: "/manageevents",
+                            state: {comedianInfo: this.props.location.state.comedianInfo}
+                          }}
+                        > Manage Events </Link> </li>
+                </ul>
+              </div>
+            </nav>
+          
+          
+          <Route path="/bookvenue" component={BookVenuePage} />
+          <Route path="/manageevents" component={ManageEventsPage} />
 
-     
-      </div>
-      </BrowserRouter>
+       
+        </div>
+        </BrowserRouter>
 
       )
+    } 
+  
+  return (<div> Please login or signup </div>)
   }
-
-}
-
+} 
 export default ComedianDash;

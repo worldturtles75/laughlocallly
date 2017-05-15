c75aea6d4960f97a4a714954d2e8171f1b55c682
Alan Zheng <aszheng12@gmail.com>
2017-05-15 15:51:03 -0700

    fixed managed events UI

--- client/components/PendingEvent.jsx
+++ client/components/PendingEvent.jsx
@@ -11,12 +11,21 @@ class PendingEvent extends React.Component {
   render() {
     console.log('pending props', this.props.pending)
     return (
+      <div>
       <div className="thumbnail">
-        <div>Name: {this.props.pending.name}</div>
-        <div>Date: {this.props.pending.date}</div>
-        <div>Detail: <img src={this.props.pending.photo_url}/> </div>
-        <button onClick={() => this.props.accept(this.props.pending.name)}>Accept</button><button onClick={() => this.props.deny(this.props.pending.name)}>Deny</button>
+        <img src={this.props.pending.photo_url} alt="..." />        
+        <div className="caption">
+          <h3>{this.props.pending.name}</h3>
+          <p><strong>Date:</strong> {this.props.pending.date}</p>
+          <p><strong>Time:</strong> {this.props.pending.start_time}</p>
+          <p><strong>Description:</strong> {this.props.pending.description}</p>
+
+          <button className="btn btn-primary btn-sm" onClick={() => this.props.accept(this.props.pending.name)}>Accept</button>
+          <button className="btn btn-primary btn-sm" onClick={() => this.props.deny(this.props.pending.name)}>Deny</button>
+        </div>
       </div>
+      </div>
+
     );
   }
 }
